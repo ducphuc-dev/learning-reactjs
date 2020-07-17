5489a494edee70ecf66e3cae94e72b2666db66fd
ducphuc-dev <ducphucit1202@gmail.com>
2020-07-17 20:46:40 +0700

    handle event: Toggle complete job and create new job

--- src/App.js
+++ src/App.js
@@ -9,40 +9,50 @@ import './App.css';
 import TodoItem from './components/TodoItem';
 
 const textHolders = ['What do you need to do today?', 'What next you want to do?', 'Type your job!']
-class App extends Component {
-  constructor() {
-    super()
-    this.listItems = [
-      {
-        title: 'For what reason would it be advisable.',
-        isCompleted: false
-      },
-      {
-        title: 'For what reason would it be advisable for me to think.',
-        isCompleted: true
-      },
-      {
-        title: 'It be advisable for me to think about business content?',
-        isCompleted: false
-      },
-      {
-        title: 'Print Statements all',
-        isCompleted: false
-      },
-      {
-        title: 'Call Rampbo',
-        isCompleted: true
-      },
-      {
-        title: 'Print bills',
-        isCompleted: false
-      }
-    ]
+class App extends Component<{}, any> {
+
+  // Avoid error: Property 'name of variable' does not exist on type 'Component's name' 
+  refNewJobItem: any;
+
+  // constructor function
+  constructor(props) {
+    super(props)
 
     // state
     this.state = {
-      placeholderForInput: 0
+      placeholderForInput: 0,
+      listItems: [
+        {
+          title: 'For what reason would it be advisable.',
+          isCompleted: false
+        },
+        {
+          title: 'For what reason would it be advisable for me to think.',
+          isCompleted: true
+        },
+        {
+          title: 'It be advisable for me to think about business content?',
+          isCompleted: false
+        },
+        {
+          title: 'Print Statements all',
+          isCompleted: false
+        },
+        {
+          title: 'Call Rampbo',
+          isCompleted: true
+        },
+        {
+          title: 'Print bills',
+          isCompleted: false
+        }
+      ]
     }
+
+    this.refNewJobItem = React.createRef();
+
+    // this.toggleComplete = this.toggleComplete.bind(this)
+    this.createNewJobItem = this.createNewJobItem.bind(this)
 
     // change state
     setInterval(() => {
@@ -52,6 +62,7 @@ class App extends Component {
     }, 2000);
   };
 
+  // change placeholder of input new job
   getPlaceholderInput(currentPosition) {
     switch (currentPosition) {
       case textHolders.length - 1:
@@ -61,8 +72,32 @@ class App extends Component {
     }
   }
 
+  // handle toggle complete job
+  toggleComplete(key) {
+    var arrListItems = this.state.listItems
+    arrListItems[key].isCompleted = !arrListItems[key].isCompleted
+    this.setState({
+      listItems: arrListItems
+    })
+  }
+
+  // create new job
+  createNewJobItem() {
+    console.log(this.refNewJobItem.current.value)
+    var arrListItems = this.state.listItems
+    arrListItems.push({
+      title: this.refNewJobItem.current.value,
+      isCompleted: false
+    })
+    this.setState({
+      listItems: arrListItems
+    })
+  }
+
+  // render
   render() {
-    const {placeholderForInput} = this.state // this syntax only useable from ES6 
+    const {placeholderForInput} = this.state // this syntax only useable from ES6
+    const {listItems} = this.state 
     return (
       <div className="App">
           <div className="page-content page-container" id="page-content">
@@ -74,17 +109,18 @@ class App extends Component {
                               <h2 className="card-title my-3">Awesome Todo list</h2>
                               <div className="add-items d-flex">
                                 {/* DMSt: demo for state */}
-                                <input type="text" className="form-control todo-list-input" placeholder={textHolders[placeholderForInput]} />
+                                <input type="text" className="form-control todo-list-input" ref={this.refNewJobItem} placeholder={textHolders[placeholderForInput]} />
                                 {/* end DMSt */}
-                                <button className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> 
+                                <button className="add btn btn-primary font-weight-bold todo-list-add-btn" onClick={this.createNewJobItem}>Add</button> 
                               </div>
                               <div className="list-wrapper">
                                 <ul className="d-flex flex-column-reverse todo-list">
                                   {
-                                    this.listItems.length > 0 && this.listItems.map((item, index) => <TodoItem item={item} key={index} />)
+                                    listItems.length > 0 && listItems.map((item, index) => <TodoItem item={item} key={index} onClick={this.toggleComplete.bind(this,index)} />)
+                                    // listItems.length > 0 && listItems.map((item, index) => <TodoItem item={item} key={index} onClick={() => this.toggleComplete(index)} />)
                                   }
                                   {
-                                  this.listItems.length == 0 &&
+                                  listItems.length === 0 &&
                                   <div className="App">
                                     <p className="alert alert-info">You have not job yet</p>
                                   </div>
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,9 +6,10 @@ class TodoItem extends Component {
     }
 
     render() {
+        const {item, onClick} = this.props
         return (
             <li className={this.props.item.isCompleted === true ? 'completed' : ''}>
-                <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" defaultChecked={this.props.item.isCompleted} /> {this.props.item.title} <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
+                <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" defaultChecked={item.isCompleted} onClick={onClick} /> {item.title} <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
             </li>
         );
     }
